026e448c0b4eabfd6817282ed053349306a989fe
janbrin <jan.brinkkemper@vng.nl>
2024-10-09 16:15:36 +0200

    s

--- js/config.js
+++ js/config.js
@@ -1,43 +1,66 @@
 let respecConfig = {
-  useLogo: true,
-  useLabel: true,
-  license: "cc-by",
-  specStatus: "DEF",
-  specType: "HR",
-  pubDomain: "dk",
-  shortName: "template",
-  publishDate: "2023-06-21",
-  publishVersion: "0.0.3",
+//  useLogo: true,
+//  useLabel: true,
+//  license: "eupl",
+  shortName: "OntInkLvns",
+  pubDomain: "cim",
+
+  // Zie de globale property 'localizationStrings/nl' voor de lijst met toegestane specificatie-types
+  specType: "im",
   
-  previousPublishVersion: "0.0.2",
-  previousPublishDate: "2022-06-24",
-  previousMaturity: "WV",
+  // Zie de globale property 'localizationStrings/nl' voor de lijst met toegestane specificatie-statussen
+  specStatus: "IO",
+  publishDate: "2024-09-12",
+  publishVersion: "0.0.1",
+
+  // Zie de globale property 'localizationStrings/nl' voor de lijst met toegestane maturities
+  //previousMaturity: "IO",
+  //previousPublishDate: "2022-06-24",
+  //previousPublishVersion: "0.0.4",
+  
   title: "Normafwijking",
-  content: {"ch01": "informative", "ch02": "", "mermaid": ""},
+//  subtitle: "Tevens een voorbeeld van een gegenereerd Respec document",
+//  content: {"mermaid": "", "ch01": "informative", "ch02": ""},
+//  authors:
+//    [
+//      {
+//        name: "Robert Melskens",
+//        company: "VNG Realisatie",
+//        companyURL: "https://vng.nl/artikelen/vng-realisatie",
+//      }
+//    ],
   editors:
     [
       {
         name: "Jan Brinkkemper",
-        company: "VNGR",
-        companyURL: "",
+        company: "VNG Realisatie",
+        companyURL: "https://vng.nl/artikelen/vng-realisatie",
       }
     ],
-  authors:
-    [
-      {
-        name: "Logius Standaarden",
-        company: "Logius",
-        companyURL: "https://github.com/Logius-standaarden",
-      }
-    ],
-  github: "https://github.com/Logius-standaarden/ReSpec-template",
+  github: "https://vng-gbi.github.io/Normafwijking",
 
+  //  maxTocLevel: 2,
 
-  // Create PDF and link to file in header (optional):
+  // Creëer PDF en link deze aan de file in de header van het html document (optioneel). Het is (nog) niet mogelijk hier een globale property van te maken:
   alternateFormats: [
       {
           label: "pdf",
-          uri: "template.pdf",
+          uri: "Normafwijking.pdf",
       },
-  ],
-};
+    ],
+  localBiblio: {
+        "MIM": {
+           "href": "https://docs.geostandaarden.nl/mim/mim/",
+           "publisher": "Geonovum",
+           "title": "MIM - Metamodel Informatie Modellering",
+           "date": "Oktober 2023",
+           "rawDate": "2023"
+        },
+        "SemVer": {
+           "href": "https://semver.org/lang/nl/",
+           "title": "Semantisch Versioneren 2.0.0",
+           "date": "December 19, 2023",
+           "rawDate": "2023"
+        },
+    },
+}
